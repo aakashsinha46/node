1fef95e8e2368eaeb176af74e44fee09978a1ac9
Aakash Sinha <sinha@Aakashs-Air.bbrouter>
2021-09-02 18:04:46 +0530

    passport login done, readme removed

---
+++ passport-login/passport-config.js
@@ -0,0 +1,35 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt =  require('bcrypt')
+
+function initialize(passport, getUserByEmail, getUserById){
+
+    const authenticateUser = async (email, password, done) => {
+        const user = getUserByEmail(email)
+        if(user == null){
+            return done(null, false, {message: 'no user with that email'})
+            // done(for any error-- no error means null, if user found then user else null, message)
+        }
+
+        try{
+            if(await bcrypt.compare(password, user.password)){
+                return done(null, user)
+            }else{
+                return done(null,false, {message: 'password incorrect!'})
+            }
+
+        }catch (err){
+            return done(err)
+        }
+    }
+
+    passport.use(new LocalStrategy({usernameField:'email'}, authenticateUser))
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id)
+    })
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
+    })
+}
+
+module.exports = initialize
